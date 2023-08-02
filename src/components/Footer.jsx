b3b0bdc085744bc03ecfbe36032654c51dead716
anshikamittal7 <anshikamittal34@gmail.com>
2023-08-02 17:25:28 +0530

    final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,83 +3,19 @@ import React from "react";
 const Footer = () => {
   return (
     <div id="footer" className="footer">
-      <div className="d-flex flex-column h-100">
-        <footer className="w-100 py-4 flex-shrink-0">
-          <div className="container py-4">
-            <div className="row gy-4 gx-5">
-              <div className="col-lg-4 col-md-6">
-                <h5 className="h1 text-white">SkillSyncHire</h5>
-                <p className="small text-muted">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt.
-                </p>
-                <p className="small text-muted mb-0">
-                  &copy; Copyrights. All rights reserved.{" "}
-                </p>
-              </div>
-              <div className="col-lg-2 col-md-6">
-                <h5 className="text-white mb-3">Quick links</h5>
-                <ul className="list-unstyled text-muted">
-                  <li>
-                    <a to="#">Home</a>
-                  </li>
-                  <li>
-                    <a href="#">About</a>
-                  </li>
-                  <li>
-                    <a href="#">Get started</a>
-                  </li>
-                  <li>
-                    <a href="#">FAQ</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-2 col-md-6">
-                <h5 className="text-white mb-3">Quick links</h5>
-                <ul className="list-unstyled text-muted">
-                  <li>
-                    <a href="#">Home</a>
-                  </li>
-                  <li>
-                    <a href="#">About</a>
-                  </li>
-                  <li>
-                    <a href="#">Get started</a>
-                  </li>
-                  <li>
-                    <a href="#">FAQ</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-4 col-md-6">
-                <h5 className="text-white mb-3">Newsletter</h5>
-                <p className="small text-muted">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
-                  do eiusmod tempor incididunt.
-                </p>
-                <form action="#">
-                  <div className="input-group mb-3">
-                    <input
-                      className="form-control"
-                      type="text"
-                      placeholder="Recipient's username"
-                      aria-label="Recipient's username"
-                      aria-describedby="button-addon2"
-                    />
-                    <button
-                      className="btn btn-primary"
-                      id="button-addon2"
-                      type="button"
-                    >
-                      <i className="fas fa-paper-plane"></i>
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </footer>
+      <h1>SyncSkillHire</h1>
+      <div className="grid-container">
+        <div className="grid-item">Anshika Mittal</div>
+        <div className="grid-item">front-end developer</div>
+        <div className="grid-item">7838846585</div>
+        <div className="grid-item">anshikamittal34@gmail.com</div>
+        <div className="grid-item">MERN stack developer</div>
+        <div className="grid-item">open for work</div>
+        <div className="grid-item">Sass</div>
+        <div className="grid-item">Github</div>
+        <div className="grid-item">React.js</div>
       </div>
+      <div className="copywrite">© 2021 Copyright:</div>
     </div>
   );
 };
